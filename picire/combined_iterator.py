c397a1cfba94dec743840e3602324e2290052772
Akos Kiss <akiss@inf.u-szeged.hu>
2017-02-14 12:19:32 +0100

    Minor improvements (consistent names, sensible defaults, value reuse)
    * Rename arguments in CombinedIterator to follow the naming scheme
      used elsewhere in the code, and add sensible defaults.
    * Add default for the atom argument of picire.call.
    * Reuse existing test builder in picire.call.

--- picire/combined_iterator.py
+++ picire/combined_iterator.py
@@ -1,26 +1,28 @@
-# Copyright (c) 2016 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2016-2017 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
 # This file may not be copied, modified, or distributed except
 # according to those terms.
 
+from . import config_iterators
+
 
 class CombinedIterator(object):
     """Callable iterator class that acts as generator when subset and complement check loops are combined."""
 
-    def __init__(self, subset_first, sub_it, compl_it):
+    def __init__(self, subset_first=True, subset_iterator=config_iterators.forward, complement_iterator=config_iterators.forward):
         """
         Initialize CombinedIterator with the basic settings describing the order of subset and complement checks
         together with the certain iterators.
 
         :param subset_first: Boolean value denoting whether the reduce has to start with the subset based approach or not.
-        :param sub_it: Reference to a generator function that provides config indices in an arbitrary order.
-        :param compl_it: Reference to a generator function that provides config indices in an arbitrary order.
+        :param subset_iterator: Reference to a generator function that provides config indices in an arbitrary order.
+        :param complement_iterator: Reference to a generator function that provides config indices in an arbitrary order.
         """
         self._subset_first = subset_first
-        self._subset_iterator = sub_it
-        self._complement_iterator = compl_it
+        self._subset_iterator = subset_iterator
+        self._complement_iterator = complement_iterator
 
     def __call__(self, n):
         """
